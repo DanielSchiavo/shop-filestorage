5d8ee020054604c58256c892f1760364c13c11a0
Daniel Schiavo Rosseto <daniel.schiavo35@gmail.com>
2024-05-30 14:40:38 -0300

    Alterado pom.xml colocando como pai o projeto raiz e agora arquivos .properties são mesclados com os arquivos .properties do submodulo shop-compartilhado

--- src/main/java/br/com/danielschiavo/shop/ShopFileStorageApplication.java
+++ src/main/java/br/com/danielschiavo/shop/ShopFileStorageApplication.java
@@ -1,6 +1,8 @@
 package br.com.danielschiavo.shop;
 
-import org.springframework.boot.SpringApplication;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -8,6 +10,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -23,11 +27,18 @@ public class ShopFileStorageApplication extends SpringBootServletInitializer {
         return application.sources(ShopFileStorageApplication.class);
     }
 	
-	public static void main(String[] args) {
-		String diretorioRaiz = System.getProperty("user.dir");
-		System.out.println("O diretório raiz do seu código Java é: " + diretorioRaiz);
-		
-		SpringApplication.run(ShopFileStorageApplication.class, args);
+	public static void main(String[] args) throws IOException {
+        org.springframework.core.io.Resource resource = new ClassPathResource("application.properties");
+        Properties props = PropertiesLoaderUtils.loadProperties(resource);
+        String activeProfile = props.getProperty("spring.profiles.active");
+        String serverPort = props.getProperty("shopfilestorage.server.port");
+        
+        String additionalConfig = "classpath:/application-" + activeProfile + "-compartilhado.properties";
+        
+        new SpringApplicationBuilder(ShopFileStorageApplication.class)
+                .properties("spring.config.additional-location=" + additionalConfig)
+                .properties("server.port=" + serverPort)
+                .run(args);
 	}
 	
 }
